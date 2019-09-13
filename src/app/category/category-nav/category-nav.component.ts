ecba1259315e2fc0a54ef02a609a3c7f2a24e58f
Sak Bran Aung <saobran.aung61@gmail.com>
2019-09-13 15:08:15 +0630

    Item Card
    -Scroll down function
    -Responsive item function
    -Card Design

--- src/app/category/category-nav/category-nav.component.ts
+++ src/app/category/category-nav/category-nav.component.ts
@@ -11,7 +11,7 @@ import { msgLabel } from "src/app/model/msgLabel";
 export class CategoryNavComponent {
   cols = 1;
   rows = 1;
-
+  drawer = "open";
   constructor(
     private breakpointObserver: BreakpointObserver,
     private appSetting: appSetting
@@ -22,7 +22,7 @@ export class CategoryNavComponent {
         if (result.matches) {
           this.cols = 6;
         } else {
-          this.cols = 2;
+          this.cols = 3;
         }
       });
     this.msgLabel = Object.assign(
@@ -32,25 +32,38 @@ export class CategoryNavComponent {
       this.appSetting.fontSession(this.appSetting.catTitle)
     );
   }
+  font() {
+    document.getElementById("zawgyi_uni").click();
+  }
   onScrollUp() {
     console.log("UP");
   }
   onScrollDown() {
+    // To call next items!!!
     console.log("DOWN");
+    this.items.push(1);
   }
   catOpen() {
     let catmenu = document.getElementById("searchtxt").style.visibility;
-    if (catmenu === "hidden") {
-      document.getElementById("searchtxt").style.visibility = "visible";
-      document.getElementById("searchbtn").style.visibility = "visible";
-      document.getElementById("cat-grid").style.visibility = "visible";
-      document.getElementById("cat-grid-close").style.visibility = "visible";
-    } else {
-      document.getElementById("searchtxt").style.visibility = "hidden";
-      document.getElementById("searchbtn").style.visibility = "hidden";
-      document.getElementById("cat-grid").style.visibility = "hidden";
-      document.getElementById("cat-grid-close").style.visibility = "hidden";
-    }
+    this.breakpointObserver
+      .observe([Breakpoints.HandsetLandscape, Breakpoints.HandsetPortrait])
+      .subscribe(result => {
+        if (result.matches) {
+          if (catmenu === "hidden") {
+            this.visible();
+          } else if (catmenu === "visible") {
+            this.hidden();
+          }
+        } else {
+          if (this.drawer == "open") {
+            this.drawer = "close";
+            this.cols = 2;
+          } else {
+            this.drawer = "open";
+            this.cols = 3;
+          }
+        }
+      });
   }
   open() {
     document.getElementById("cart").click();
@@ -64,10 +77,27 @@ export class CategoryNavComponent {
         if (result.matches) {
           document.getElementById("searchtxt").style.minWidth = "150px";
           document.getElementById("searchtxt").style.maxWidth = "150px";
+          this.hidden();
         } else {
           document.getElementById("searchtxt").style.minWidth = "300px";
           document.getElementById("searchtxt").style.maxWidth = "300px";
+          this.visible;
         }
       });
+    this.drawer = "open";
+  }
+
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  visible() {
+    document.getElementById("searchtxt").style.visibility = "visible";
+    document.getElementById("searchbtn").style.visibility = "visible";
+    document.getElementById("cat-grid").style.visibility = "visible";
+    document.getElementById("cat-grid-close").style.visibility = "visible";
+  }
+  hidden() {
+    document.getElementById("searchtxt").style.visibility = "hidden";
+    document.getElementById("searchbtn").style.visibility = "hidden";
+    document.getElementById("cat-grid").style.visibility = "hidden";
+    document.getElementById("cat-grid-close").style.visibility = "hidden";
   }
 }
