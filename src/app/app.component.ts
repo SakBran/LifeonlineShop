3e4ae7baa4670035c6c9dd9a51a852ebbeef1a55
Sak Bran Aung <saobran.aung61@gmail.com>
2019-09-13 17:03:30 +0630

    Font auto change
    --Compelte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'onlineShop';
+  title = "onlineShop";
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.checkfont();
+  }
+
+  checkfont() {
+    const el = document.createElement("canvas");
+    const ctx = el.getContext("2d");
+
+    const kaWidth = ctx.measureText("က").width;
+    const patSintWidth = ctx.measureText("က္က").width;
+    if (kaWidth === patSintWidth) {
+      let font = "font";
+      localStorage.setItem(font, "u");
+    } else {
+      let font = "font";
+      localStorage.setItem(font, "z");
+    }
+  }
 }
